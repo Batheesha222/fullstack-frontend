fc8d88affbeba7476fd7ec72f4d220e0b9625560
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-07-06 22:05:27 +0530

    Add user Route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './layout/Navbar.js';
 import Home from './pages/Home.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddUser from './users/AddUser.js';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
+
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/adduser" element={<AddUser />} />
+        </Routes>
+
+
+      </Router>
+
     </div>
   );
 }
